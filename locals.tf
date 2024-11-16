e0ffcb51b91886175d7774b8718c09069d34bffa
zoki <tosevzoran@gmail.com>
2024-11-16 18:12:32 +0100

    feat: setup github workspace

--- locals.tf
+++ locals.tf
@@ -10,7 +10,15 @@ locals {
       description = "FrontEndMasters Devops workspace"
       execution_mode = "remote"
       project_id = module.project["fem-eci-project"].id
-      vcs_repo_identifier = "tosevzoran/fem-terraform-eci-tfe"
+      vcs_repo_identifier = "${var.github_organization_name}/fem-terraform-eci-tfe"
+    }
+
+
+    "fem-eci-github" = {
+      description = "FrontEndMasters Github workspace"
+      execution_mode = "local"
+      project_id = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-terraform-eci-github"
     }
   }
 }
